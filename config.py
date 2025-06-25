317d1873327bc51c436e0d7bc48ae9da58ff2e0f
fr10pro <realemonfx@gmail.com>
2025-06-26 02:27:02 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+API_ID = 28593211
+API_HASH = '27ad7de4fe5cab9f8e310c5cc4b8d43d'
+BOT_TOKEN = '7025826430:AAFtNhgFpTjh3eB6k2flrelKSPB5Da5WX6I'
+ADMIN_ID = 5559075560  # Your Telegram Admin ID
+DATABASE_NAME = 'channels.db'
+POLLING_INTERVAL = 300 # seconds (5 minutes) for channel polling
